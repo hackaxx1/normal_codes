7b548fa5cdf7f945976fb5f8a5d268f0d5b71ebf
hackaxx1 <77045082+hackaxx1@users.noreply.github.com>
2025-02-15 09:25:59 +0300

    Update change_password.py
    Arayüz yapıleı

--- profile/sub-pages/change_password.py
+++ profile/sub-pages/change_password.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write("şifre değiştirme sayfası")
+indb_password = "1234"
+
+old_password = st.text_input("Eski Şifre", type="password")
+new_password = st.text_input("Yeni Şifre", type="password")
+if st.button("Şifreyi Güncelle"):
+    if old_password == indb_password:
+        st.success("✅ Şifre başarıyla değiştirildi!")  # Gerçek bir değişiklik için burada veri kaydedilmeli.
+    else:
+        st.error("❌ Eski şifre yanlış!")
